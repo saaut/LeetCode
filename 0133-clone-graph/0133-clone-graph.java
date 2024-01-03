efd5be01278a0070a7db265fd832f97858bb635b
saaut <109278065+saaut@users.noreply.github.com>
2024-01-03 16:03:47 +0900

    Time: 27 ms (51.09%), Space: 42.8 MB (10.61%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -26,23 +26,28 @@ class Solution {
     public Node cloneGraph(Node node) {
     if(node==null)
         return null;
-    Map<Node,Node> clones=new HashMap<>();
-    return dfs(node,clones);
-    }
-    
-    
-    public Node dfs(Node node,Map<Node,Node>clones){
-    if(clones.containsKey(node)){
-        return clones.get(node);
-    }
-    Node clone=new Node(node.val,new ArrayList<Node>());
-    clones.put(node,clone);
         
-     for(Node n:node.neighbors){
-         clone.neighbors.add(dfs(n,clones));
-     }   
-        
+    Node clone=new Node(node.val);
+    Map<Node,Node> clones=new HashMap<>();//node와 clone을 비교하기 위한 clones
+    clones.put(node,clone);  
+    
+    Queue<Node> queue = new ArrayDeque<>();
+    queue.offer(node);
+     
+    while(!queue.isEmpty()){
+        Node current = queue.poll();
+        //노드 삭제하고
+        //노드에 이웃이 있는지 확인
+        for(Node n: current.neighbors){
+            if(!clones.containsKey(n)){//이웃 중복검사
+
+                queue.offer(n);//이웃들 큐에 추가
+                Node neighborClone=new Node(n.val);//이웃을 복제. value
+                clones.put(n,neighborClone);//목제한 이웃을 clones에 넣음.
+            }
+          clones.get(current).neighbors.add(clones.get(n));
+        }
+    }
         return clone;
-        
     }
 }
