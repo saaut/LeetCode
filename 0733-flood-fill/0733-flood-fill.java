d45be7d97d74af3a0481cdf60c40ef64756837a2
saaut <109278065+saaut@users.noreply.github.com>
2024-01-03 14:43:16 +0900

    Time: 4 ms (5.80%), Space: 44.5 MB (33.89%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,21 +1,38 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if(image[sr][sc]==color)
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if (image[sr][sc] == newColor) {
             return image;
-        dfs(image,sr,sc,image[sr][sc],color);
+        }
+
+        int oldColor = image[sr][sc];
+        image[sr][sc] = newColor;
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{sr, sc});
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int row = current[0];
+            int col = current[1];
+
+            int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+            for (int[] dir : directions) {
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+
+                if (newRow >= 0 && newRow < image.length && newCol >= 0 && newCol < image[0].length) {
+                    if (image[newRow][newCol] == oldColor) {
+                        image[newRow][newCol] = newColor;
+                        queue.offer(new int[]{newRow, newCol});
+                    }
+                }
+            }
+        }
+
         return image;
     }
-    public void dfs(int[][] image,int row, int column, int oldColor, int newColor){
-        if(row<0||column<0||row>image.length-1||column>image[0].length-1||image[row][column]!=oldColor)
-            return;
-        
-        image[row][column]=newColor;
-        
-        dfs(image,row,column+1,oldColor,newColor);
-        dfs(image,row,column-1,oldColor,newColor);
-        dfs(image,row+1,column,oldColor,newColor);
-        dfs(image,row-1,column,oldColor,newColor);
-    }
-    
-    
 }
