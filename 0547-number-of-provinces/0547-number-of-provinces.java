e05942e084fdf094657e548a82e606c89b4fc15d
saaut <109278065+saaut@users.noreply.github.com>
2024-01-09 15:50:24 +0900

    Time: 2 ms (54.74%), Space: 48.2 MB (6.26%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,24 +1,21 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        Set<Integer> visited=new HashSet<>();
+        Set<Integer> visited = new HashSet<>();
         int count=0;
         
         for(int i=0;i<isConnected.length;i++){
             if(!visited.contains(i)){
-                count++;
-                dfs(i,isConnected,visited);
+            dfs(i,isConnected,visited);
+            count++;
             }
         }
         return count;
     }
-    
-    public void dfs(int node,int[][] isConnected,Set<Integer>visited){
-        visited.add(node);
-        for(int adj=0;adj<isConnected[node].length;adj++){
-            if(!visited.contains(adj)&&isConnected[node][adj]==1){
-                dfs(adj,isConnected,visited);
-            }
+    public void dfs(int i,int[][] isConnected,Set<Integer> visited){
+        visited.add(i);
+        for(int j=0;j<isConnected.length;j++){
+            if(isConnected[i][j]==1&&!visited.contains(j))
+                dfs(j,isConnected,visited);
         }
-        
     }
 }
